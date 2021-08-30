d9dcd64340c3765c61927a97459630a904d4b46e
Vera <88088044+vnievelstein@users.noreply.github.com>
2021-08-30 11:24:57 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,13 +1,13 @@
 // Opdracht 2 Java Script.
 // Zeg gedag tegen de studenten
-console.log("Hallo Winc Academy studenten")
+console.log("Hallo Winc Academy studenten");
 // 1. Rekenopdracht verminigvuldiging en de uitkomst loggen.
 /* 5. let multiplication = 1230941 * 1823794;
-console.log("Bereken 1230941 * 1823794:")
-console.log(multiplication)
+console.log("Bereken 1230941 * 1823794:");
+console.log(multiplication);
 // 2. Rekenopdracht delen met uitkomst loggen.
 let equation = 637263/54;
-console.log("Bereken 637263/54:")
+console.log("Bereken 637263/54:");
 console.log(equation) */
 /*vraag 4. ik zie de uitkomst van de comment niet, want comment is alleen 
 commentaar voor de verduidelijking van het waarom van de code */
