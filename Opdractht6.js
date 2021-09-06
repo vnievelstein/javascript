2ddec447e7fb3d324d0aaf6de1069ff524cf0ad0
Vera <88088044+vnievelstein@users.noreply.github.com>
2021-09-06 22:16:53 +0200

    Create Opdractht6.js

---
+++ Opdractht6.js
@@ -0,0 +1,31 @@
+//Opdracht 6, deel 1 define a function that makes pizza
+const makePizza = function(){
+    console.log("Zet de oven aan op 220 graden");
+    console.log("Haal de pizza uit de vriezer");
+    console.log("Haal de pizza uit de verpakking");
+    console.log("Stop als de oven 10 minuten aan staat de pizza in de oven");
+    console.log("Haal na 20 minuten de pizza uit de oven");
+    console.log("Leg de pizza op een bord en serveer de pizza");
+};
+makePizza();
+makePizza();
+makePizza();
+console.log("----------------Deel 2----------------");
+//Opdracht 6, deel 2: define a function that makes sushi
+const makeSushi = function(){
+    console.log("Kook de rijst volgens de instructie");//dat zou ook een functie kunnen zijn
+    console.log("Snijd de vis op het juiste formaat");
+    console.log("Week het zeewier");
+    console.log("Leg het zeewier op het rieten matje");
+    console.log("Smeer het zeewier in met een dun laagje Wasabi");
+    console.log("leg de rijst in een laag op het zeewier");
+    console.log("Leg de vis op de rijst");
+    console.log("Rol alle tot een rol in het rieten matje");
+    console.log("Snijd de rol in plakken van 1,5 centimeter");
+};
+//Opdracht 6, deel 3: maak drie keer sushi en twee keer pizza
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
