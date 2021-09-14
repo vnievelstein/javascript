43723896d847658744efd563b60ebf1e36678123
Vera <88088044+vnievelstein@users.noreply.github.com>
2021-09-14 18:26:55 +0200

    Update opdrachtTien.js

--- opdrachtTien.js
+++ opdrachtTien.js
@@ -24,7 +24,7 @@ const welkom = function() {
 };
  welkom();
 //bovenstaand geeft ook 'Hey Kiddo' als je iets anders dan een getal invult
-
+console.log("-------------------------------");
 //Opdracht 10 deel 2. VAT calculations
 //schrijf een functie die de basis prijs en het vat percentage neemt
 //en de prijs inclusief vat returns. het moet uit twee functies bestaan
@@ -38,13 +38,34 @@ const vatCalculation = function(basePrice, vatPercentage){
 
 };
 //vatCalculation(1000,21);
-console.log("Het totaalbedrag inclusief VAT is ")
+//console.log("Het totaalbedrag inclusief VAT is ")
 const priceInclVat = function() {
+    console.log("Het totaalbedrag inclusief VAT is ");
 vatCalculation(150,21);
 }
 priceInclVat();
+console.log("-------------------------------");
 //Opdracht 10 deel 3, VAT exercise 2
 //Neem de prijs inclusief VAT en maak een functie die de twee
 //opsplitst in een array met twee getallen; de basisprijs en de VAT prijs
 //gebruik twee functies waarbij de ene funcie de andere aanroept om een
 //deel van de berekening uit te voeren. 
+
+//bereken het btw-bedrag en return de prijs inclusief btw en het btw -bedrag
+const berekenBtw = function(prijsInclBtw, btwPercentage){
+const btwBedrag = prijsInclBtw * btwPercentage/(100 + btwPercentage);
+//console.log("Het bedrag aan btw is " ); 
+return [prijsInclBtw,btwBedrag];
+}
+//roep de vorige functie aan om het bedrag incl. btw en de btw op te vragen.
+// haal deze van elkaar af om het basisbedrag te krijgen en print het resultaat.
+
+const berekenBasisPrijs = function(){
+    const resultaat = berekenBtw(125,25);
+    //console.log(resultaat);
+    const resultaat2 = resultaat[0] - resultaat[1];
+    console.log("Het basisbedrag is " + resultaat2);
+};
+
+berekenBasisPrijs(); // roep de functie aan om het resultaat te krijgen. 
+
